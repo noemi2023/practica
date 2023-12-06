091b809f2730bbf32599a6b12bb80f378514edbf
noemi2023 <noemibonasera@yahoo.com>
2023-12-06 17:51:38 -0300

    ayuda

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundo')
+  res.render('index')
 })
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
